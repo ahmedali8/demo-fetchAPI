3178abd0888968edc3f3c4c2742c2a5baa00707c
ahmedali8 <imahmedalibhatti@gmail.com>
2020-08-14 10:55:41 +0500

    class-8: demo local JS  fetch() and useEffect() hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let data = { title: 'Waiting for Data...' };
+	const [todo, setTodo] = useState(data);
+	const [isData, setData] = useState(false);
+	const [isFetching, setFetching] = useState(false);
+
+
+
+	useEffect(() => {
+		async function fetchData() {
+			setFetching(true);
+			const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+			console.log("Response: ", response);
+
+			const data2 = await response.json();
+			setTodo(data2);
+
+			setFetching(false);
+			console.log("Data: ", todo);
+		}
+		fetchData();
+	}, [isData]);
+
+	if (isFetching) {
+		return <div>Data loading...</div>
+	}
+
+
+	return (
+		<div>
+			<span>Title: {data.title}</span>
+			<br />
+			<span>Title: {todo.title}</span>
+		</div>
+	);
 }
 
 export default App;
